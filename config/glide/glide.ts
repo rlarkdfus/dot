49f0d80a64eef145dfc6c514db42d8979a4d541b
Gordon Kim <gordonkimor@gmail.com>
2025-10-09 11:59:49 -0400

    ignore mode tabs

--- config/glide/glide.ts
+++ config/glide/glide.ts
@@ -35,3 +35,6 @@ glide.autocmds.create("UrlEnter", /https:\/\/www\.overleaf\.com/, async (e) => {
   await glide.excmds.execute("mode_change ignore");
   return async () => await glide.excmds.execute("mode_change normal");
 });
+
+glide.keymaps.set("ignore", "<C-j>", "tab_next");
+glide.keymaps.set("ignore", "<C-k>", "tab_prev");
